a9d4ae8e9307d3488ca8882f35933a2c13ea7987
HASWELL <eh1622@att.com>
2021-10-25 10:13:43 -0500

    separate HEADER to its own component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//this component contains the top header bar
+class Header extends React.Component{
+	render(){
+  		return(
+           <div className="list-books-title">
+              <h1>E Allen Haswell -  MyReads</h1>
+            </div>
+        );
+  }
+}
+
+export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchButton from './SearchButton';
+import Header from './Header';
 
 
 
@@ -20,9 +21,7 @@ class Home extends React.Component{
 	render() {
     	return(
                 <div className="list-books">
-                  <div className="list-books-title">
-                    <h1>MyReads</h1>
-                  </div>
+                  <Header />
                   <div className="list-books-content">
                     <div>
                       <div className="bookshelf">
