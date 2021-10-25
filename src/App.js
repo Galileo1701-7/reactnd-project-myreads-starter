17b30583e9c350e15c9da0a6fe281e2d54442c14
HASWELL <eh1622@att.com>
2021-10-25 10:39:47 -0500

    refactor by adding BOOKSDISPLAY comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Home from "./components/Home"; 
 import { BrowserRouter, Route } from 'react-router-dom';
@@ -7,21 +7,26 @@ import Search from './components/Search';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    currentBooksOnShelves: []
   }
 
+  //on page load, this code gets the current book/shlef info from the backend
+  componentDidMount(){
+    BooksAPI.getAll().then(resp => this.setState({ currentBooksOnShelves: resp }))
+    
+  }; 
+
+  
+
   render() {
+    console.log('app.js =',this.state.currentBooksOnShelves); 
+
+
     return (
       <BrowserRouter>
         <div className="app">
         <Route exact path='/' render={() => (
-          <Home />
+          <Home currentBooksOnShelves={this.state.currentBooksOnShelves}/>
         )}/>
         <Route path='/search' render={({ history }) => (
         	<Search />        
