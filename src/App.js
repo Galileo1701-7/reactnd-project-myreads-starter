e744919cf086ee42d28941b17f385a9ef5a54508
HASWELL <eh1622@att.com>
2021-10-26 11:08:28 -0500

    add ability to assign shelf from SEARCH page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import Home from "./components/Home"; 
 import { BrowserRouter, Route } from 'react-router-dom';
 import Search from './components/Search';
+//import {shelfChangeExisting} from './util/changeShelves'
 
 class BooksApp extends React.Component {
   state = {
@@ -11,12 +12,14 @@ class BooksApp extends React.Component {
   }
 
   //change the shelf of book (from dropdown on SHELF component)
+  //check to see if the book is on the shelf, if so update it... if not add the book with updated shelf
   changeShelf = (book, newShelf) => {
     const shelvedBook = this.state.currentBooksOnShelves.find(
       shelvedBook => shelvedBook.id === book.id      
     );
     console.log('shelved book =', shelvedBook);
       if (shelvedBook) {
+        //shelfChangeExisting(book, newShelf, BooksAPI);
         console.log('change shelf of ', book, ' to ', newShelf);
         //update book on backend
         BooksAPI.update(book,newShelf);
@@ -26,11 +29,20 @@ class BooksApp extends React.Component {
             if (shelvedBook.id === book.id) {
               shelvedBook.shelf = newShelf;
             } 
-            return shelvedBook;
+            return shelvedBook;            
           })
       }));    
       } else {
-        console.log('This book is not currently on any shelf');
+        console.log(book.title, ' is not currently on any shelf. Moving to ', newShelf);
+        //update backend
+        BooksAPI.update(book,newShelf);
+         //update State
+        this.setState(({ currentBooksOnShelves }) => {         
+          book.shelf = newShelf;
+          currentBooksOnShelves.push(book);
+          return {currentBooksOnShelves: currentBooksOnShelves};
+        });
+        
       }  
   } 
   
@@ -55,7 +67,7 @@ class BooksApp extends React.Component {
             changeShelf={this.changeShelf}/>
         )}/>
         <Route path='/search' render={({ history }) => (
-        	<Search />        
+        	<Search changeShelf={this.changeShelf}/>        
         )}/>
         </div>
       </BrowserRouter>
